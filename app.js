14100ffa8070f59c2d56e96094129973471aad64
Sean Cahill <seancahill25@gmail.com>
2018-02-07 16:32:02 +0000

    finished section1

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 var express = require("express");
 var app = express();
 var path = require("path");
+var bodyParser = require("body-parser");
 
 var routes = require("./api/routes");
 
 app.set("port", process.env.PORT);
 
-app.use("/css", function(req, res, next) {
+app.use(function(req, res, next) {
     console.log(req.method, req.url);
     next();
 });
 
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(bodyParser.urlencoded({ extended: false}));
 
 app.use('/api', routes);
 
