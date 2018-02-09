a9564008ebf9b399bbed5a5178a1af1917115b3b
Sean Cahill <seancahill25@gmail.com>
2018-02-09 22:19:27 +0000

    added connection to mongodb

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -4,28 +4,40 @@ var hotelData = require("../data/hotel-data.json");
 module.exports.hotelsGetAll = function(req, res) {
     
         var db = dbconn.get();
+        var collection = db.collection("hotels");
         
-        console.log("db", db);    
-    
-        console.log("GET the hotels");
-        console.log("req.query");
-        
-        var offset = 0;
-        var count = 5;
-        
-        if (req.query && req.query.offset) {
-            offset = parseInt(req.query.offset, 10);
-        }
-        
-         if (req.query && req.query.count) {
-            count = parseInt(req.query.count, 10);
-        }
-        
-        var returnData = hotelData.slice(offset,offset+count);
-        
+         collection
+         .find()
+         .toArray(function(err, docs) {
+             console.log("found hotels", docs);
         res
-            .status(200)
-            .json( {returnData} );
+        .status(200)
+        .json(docs);
+         });
+        
+        
+        
+        // console.log("db", db);    
+    
+        // console.log("GET the hotels");
+        // console.log("req.query");
+        
+        // var offset = 0;
+        // var count = 5;
+        
+        // if (req.query && req.query.offset) {
+        //     offset = parseInt(req.query.offset, 10);
+        // }
+        
+        //  if (req.query && req.query.count) {
+        //     count = parseInt(req.query.count, 10);
+        // }
+        
+        // var returnData = hotelData.slice(offset,offset+count);
+        
+        // res
+        //     .status(200)
+        //     .json( returnData );
 };
 
 module.exports.hotelsGetOne = function(req, res) {
