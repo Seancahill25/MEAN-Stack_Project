237a2c398e82a64b28cc5d4a1d1019617de2e603
Sean Cahill <seancahill25@gmail.com>
2018-02-11 19:56:27 +0000

    finished section 2

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -1,26 +1,11 @@
 var dbconn = require("../data/dbconnection.js");
+var ObjectId = require("mongodb").ObjectId
 var hotelData = require("../data/hotel-data.json");
 
 module.exports.hotelsGetAll = function(req, res) {
     
         var db = dbconn.get();
         var collection = db.collection("hotels");
-        
-         collection
-         .find()
-         .toArray(function(err, docs) {
-             console.log("found hotels", docs);
-        res
-        .status(200)
-        .json(docs);
-         });
-        
-        
-        
-        console.log("db", db);    
-    
-        console.log("GET the hotels");
-        console.log("req.query");
         
         var offset = 0;
         var count = 5;
@@ -33,28 +18,62 @@ module.exports.hotelsGetAll = function(req, res) {
             count = parseInt(req.query.count, 10);
         }
         
-        var returnData = hotelData.slice(offset,offset+count);
-        
+         collection
+         .find()
+         .skip(offset)
+         .limit(count)
+         .toArray(function(err, docs) {
+             console.log("found hotels", docs);
         res
-            .status(200)
-            .json( returnData );
+        .status(200)
+        .json(docs);
+         });
 };
 
 module.exports.hotelsGetOne = function(req, res) {
+        var db = dbconn.get();
+        var collection = db.collection("hotels");
+    
         var hotelId = req.params.hotelId;
-        var thisHotel = hotelData[hotelId];
         console.log("GET hotelId", hotelId);
-        res
+        
+        collection
+        .findOne({
+            _id : ObjectId(hotelId)
+        }, function(err, doc) {
+            res
             .status(200)
-            .json( {thisHotel} );
+            .json( doc );
+        });
+
 };
 
 module.exports.hotelsAddOne = function(req, res) {
+    var db = dbconn.get();
+    var collection = db.collection("hotels");
+    var newHotel;
+    
     console.log("POST new hotel");
-    console.log(req.body);
-    res
-        .status(200)
-        .json(req.body);
+    
+    if (req.body && req.body.name && req.body.stars) {
+        newHotel = req.body;
+        newHotel.stars = parseInt(req.body.stars, 10);
+        
+        collection.insertOne(newHotel, function(err, res) {
+            console.log(res);
+            console.log(res.ops)
+             res
+            .status(201)
+            .json(res.ops);
+        });
+       
+    } else {
+        console.log("Data missing from body");
+        res 
+        .status(400)
+        .json({ message : "Required data missing from body"});
+    }
+    
 };
 
 
