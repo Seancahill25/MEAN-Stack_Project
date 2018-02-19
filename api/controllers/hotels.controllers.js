2b53a09cdaaa264d254900cfbb7663b583633037
Sean Cahill <seancahill25@gmail.com>
2018-02-19 22:55:07 +0000

    added error checking

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -33,6 +33,7 @@ module.exports.hotelsGetAll = function(req, res) {
         
         var offset = 0;
         var count = 5;
+        var maxCount = 10;
         
         if (req.query && req.query.lat && req.query.lng) {
             runGeoQuery(req, res);
@@ -47,15 +48,41 @@ module.exports.hotelsGetAll = function(req, res) {
             count = parseInt(req.query.count, 10);
         }
         
+        if (isNaN(offset) || isNaN(count)) {
+            res
+            .status(400)
+            .json({
+                "message" : "If supplied in querystring count and offset should be numbers"
+            });
+            return;
+        }
+        
+        if (count > maxCount) {
+            res
+            .status(400)
+            .json({
+                "message" : "Conut limit of " + maxCount + " exceeded"
+            });
+            return;
+        }
+        
         Hotel
         .find()
         .skip(offset)
         .limit(count)
         .exec(function(err, hotels) {
+            if (err) {
+                console.log("Error finding hotels");
+                res
+                .status(500)
+                .json(err);
+            } else {
             console.log("found hotels", hotels.length);
             res
-            .json(hotels);
-        })
+              .json(hotels);
+            }
+         
+        });
         
 };
 
@@ -67,9 +94,23 @@ module.exports.hotelsGetOne = function(req, res) {
         Hotel
         .findById(hotelId)
         .exec(function(err, doc) {
+            var response = {
+                status : 200,
+                message : doc
+            };
+            if (err) {
+                console.log("Error finding hotel");
+                response.status = 500;
+                response.message = err;
+            } else if(!doc) {
+                response.status(404);
+                response.message = {
+                    "message" : "Hotel ID not found"
+                };
+            }  
             res
-            .status(200)
-            .json( doc );
+            .status(response.status)
+            .json(response.message);
         });
 
 };
